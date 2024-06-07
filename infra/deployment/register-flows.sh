dda143393be92b244567b62b23bbd65e14c56c44
Vincent <vincent.chery@m4x.org>
2024-06-07 16:24:38 +0200

    Fix deployment environment variables

--- infra/deployment/register-flows.sh
+++ infra/deployment/register-flows.sh
@@ -2,10 +2,11 @@
 
 docker run -t --rm --network=host --name forklift-register-flows \
         -v /opt2/sacrois-data:/opt2/sacrois-data \
-        -v "$(pwd)"/.env:/home/forklift/data_warehouse/.env \
+        -v "$(pwd)"/.env:/home/forklift/forklift/.env \
 	    -v "$(pwd)"/backend.toml:/home/forklift/.prefect/backend.toml \
         --env-file .env \
         -e FORKLIFT_VERSION \
+        -e FORKLIFT_DOCKER_IMAGE \
         -e SACROIS_FILES_GID \
-        ghcr.io/mtes-mct/fisheries-and-environment-data-warehouse/forklift:$FORKLIFT_VERSION \
+        $FORKLIFT_DOCKER_IMAGE:$FORKLIFT_VERSION \
         python forklift/main.py
