8999fd6a5b3a811e7cb240b4aadbd1f6dcb880d9
Quentin <quentin.loridant@protonmail.com>
2025-09-03 16:43:04 +0200

    Baser la query analytics sur mission_general_info

--- forklift/forklift/pipeline/queries/rapportnav_remote/analytics_missions_full_data.sql
+++ forklift/forklift/pipeline/queries/rapportnav_remote/analytics_missions_full_data.sql
@@ -1,4 +1,2 @@
-SELECT rapportnav_proxy.mission_action.mission_id,  
-FROM rapportnav_proxy.mission_action
-LEFT JOIN rapportnav_proxy.mission_general_info on rapportnav_proxy.mission_general_info.mission_id = rapportnav_proxy.mission_action.mission_id
-WHERE rapportnav_proxy.mission_action.is_complete_for_stats::int = 1;
+SELECT rapportnav_proxy.mission_general_info.id, rapportnav_proxy.mission_general_info.mission_id, rapportnav_proxy.mission_general_info.consumed_fuel_in_liters
+FROM rapportnav_proxy.mission_general_info;
