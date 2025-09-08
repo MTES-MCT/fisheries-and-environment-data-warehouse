25f360f1eff34de64122adaa3419f512f4d8d76a
Quentin Loridant <quentin-loridant@device-245.home>
2025-09-08 10:52:34 +0200

    MAJ requete SQL : rapportnav & fish

--- forklift/forklift/pipeline/queries/rapportnav_remote/analytics_missions_full_data.sql
+++ forklift/forklift/pipeline/queries/rapportnav_remote/analytics_missions_full_data.sql
@@ -1,28 +1,21 @@
 
 WITH all_actions AS (
-    SELECT rapportnav_proxy.mission_action.id, 
-        rapportnav_proxy.mission_action.mission_id as mission_id,
+    SELECT rapportnav_proxy.mission_action.mission_id::Int32 as mission_id,
         'from_rapportnav' as source
     FROM rapportnav_proxy.mission_action
     UNION ALL 
-    SELECT monitorfish_proxy.mission_actions.id, 
-        monitorfish_proxy.mission_actions.mission_id as mission_id,
+    SELECT monitorfish_proxy.mission_actions.mission_id::Int32 as mission_id,
         'from_monitorfish' as source
     FROM monitorfish_proxy.mission_actions
-    UNION ALL
-    SELECT monitorenv_proxy.mission_actions.id,
-        monitorenv_proxy.mission_actions.id as mission_id,
-        'from_monitorenv' as source
-    FROM monitorenv_proxy.mission_actions
 ),
 aggregated_actions_by_missions AS (
-    SELECT COUNT(*)
+    SELECT mission_id, COUNT(*) as cmpt
     FROM all_actions
     GROUP BY mission_id
 )
 
 
-SELECT rapportnav_proxy.mission_general_info.id, rapportnav_proxy.mission_general_info.mission_id, rapportnav_proxy.mission_general_info.consumed_fuel_in_liters
+SELECT rapportnav_proxy.mission_general_info.mission_id::Int32 as "id", rapportnav_proxy.mission_general_info.consumed_fuel_in_liters::Int32,aggregated_actions_by_missions.cmpt
 FROM rapportnav_proxy.mission_general_info
 LEFT JOIN aggregated_actions_by_missions on aggregated_actions_by_missions.mission_id = rapportnav_proxy.mission_general_info.mission_id
 ;
