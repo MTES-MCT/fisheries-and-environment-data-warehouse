6e507f3213c0014ba6021184901f4e6948cdae95
Vincent <vincent.chery@m4x.org>
2025-09-10 18:11:10 +0200

    Fix non overlapping FAO areas

--- forklift/forklift/pipeline/queries/monitorfish_remote/non_overlapping_fao_areas.sql
+++ forklift/forklift/pipeline/queries/monitorfish_remote/non_overlapping_fao_areas.sql
@@ -12,6 +12,6 @@ LEFT JOIN fao_areas f2
 ON  
     ST_Intersects(f.wkb_geometry, f2.wkb_geometry) AND
     f2.f_code != f.f_code AND
-    ST_Area(ST_Intersection(f.wkb_geometry, f2.wkb_geometry)::geography) >= 0.95 * ST_Area(f2.wkb_geometry::geography)
+    ST_Area(ST_Intersection(f.wkb_geometry, f2.wkb_geometry)::geography) >= 0.99 * ST_Area(f2.wkb_geometry::geography)
 GROUP BY 1
 HAVING COALESCE(ST_Area(ST_Union(ST_Intersection(f.wkb_geometry, f2.wkb_geometry))::geography) / ST_Area(f.wkb_geometry::geography), 0) < 0.99
