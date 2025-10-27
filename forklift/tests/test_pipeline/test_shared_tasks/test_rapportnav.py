9cf00ef5ad43b010836bf3feffc259e92adec50e
Quentin Loridant <q.loridant@gmail.com>
2025-10-27 10:20:56 +0100

    Test data similaire à la production

--- forklift/tests/test_pipeline/test_shared_tasks/test_rapportnav.py
+++ forklift/tests/test_pipeline/test_shared_tasks/test_rapportnav.py
@@ -8,20 +8,31 @@ HEADERS = {}
 from forklift.pipeline.shared_tasks.rapportnav import _default_to_df, fetch_rapportnav_api
 
 
-def test_default_to_df_with_flat_payload():
-    payload = [{"a": 1}, {"a": 2}]
-    df = _default_to_df(payload)
-    assert isinstance(df, pd.DataFrame)
-    assert df.shape[0] == 2
-
-
 @patch("forklift.pipeline.shared_tasks.rapportnav.load_to_data_warehouse")
 @patch("forklift.pipeline.shared_tasks.rapportnav.requests.get")
 def test_fetch_rapportnav_api(mock_requests_get, mock_load):
     # Mock response
     mock_response = MagicMock()
+           
     mock_response.raise_for_status.return_value = None
-    mock_response.json.return_value = [{"a": 1}, {"a": 2}]
+    mock_response.json.return_value = [
+        {
+            "id": 1,
+            "idUUID": 1,
+            "serviceId": 1,
+            "startDateTimeUtc": "2024-01-01",
+            "endDateTimeUtc": "2024-12-12",
+            "missionTypes": "missionTypes",
+            "controlUnits": "controlUnits",
+            "facade": "facade",
+            "isDeleted": False,
+            "missionSource": "missionSource",
+            "data": {
+                "indicateur1": 1,
+                "indicateur2": 2
+            }
+        }
+    ]
     
     mock_requests_get.return_value = mock_response
 
