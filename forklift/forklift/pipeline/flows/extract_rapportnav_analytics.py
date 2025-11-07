47e1cd02dd830e93186c81c552d62d7db40089b3
Quentin Loridant <q.loridant@gmail.com>
2025-11-07 11:35:10 +0100

    Create flow for RapportNav API

---
+++ forklift/forklift/pipeline/flows/extract_rapportnav_analytics.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+import prefect
+from prefect import Flow, case, unmapped
+
+from forklift.pipeline.shared_tasks.rapportnav import fetch_rapportnav_api, extract_missions_ids
+from forklift.pipeline.shared_tasks.control_flow import check_flow_not_running
+from forklift.pipeline.shared_tasks.generic import (
+    create_database_if_not_exists,
+)
+
+
+with Flow("RapportNavAnalytics") as flow:    
+    logger = prefect.context.get("logger")
+
+    flow_not_running = check_flow_not_running()
+    with case(flow_not_running, True):
+    
+        create_database = create_database_if_not_exists("rapportnav")
+        mission_ids = extract_missions_ids()
+        aem_analytics = fetch_rapportnav_api.map(
+            path='analytics/v1/patrol',
+            missions_ids=unmapped(extract_missions_ids)
+        )
+
+flow.file_name = Path(__file__).name
