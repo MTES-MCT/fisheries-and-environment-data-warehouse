c85639d93050fc9e103a5d20fdce8f20ecefbaa9
Quentin Loridant <q.loridant@gmail.com>
2025-11-07 11:55:19 +0100

    Maj .env et format test data

--- forklift/tests/test_pipeline/test_shared_tasks/test_rapportnav.py
+++ forklift/tests/test_pipeline/test_shared_tasks/test_rapportnav.py
@@ -10,34 +10,36 @@ from forklift.pipeline.shared_tasks.rapportnav import fetch_rapportnav_api, extr
 
 def test_extract_missions_ids(add_monitorenv_proxy_database):
     mission_ids = extract_missions_ids.run()
-    assert mission_ids
+    assert len(mission_ids) > 0 
     
-@patch("forklift.pipeline.shared_tasks.rapportnav.requests.get")
-def test_fetch_rapportnav_api(mock_requests_get):
+@patch("forklift.pipeline.shared_tasks.rapportnav.requests.post")
+def test_fetch_rapportnav_api(mock_requests_post):
     # Mock response
     mock_response = MagicMock()
            
     mock_response.raise_for_status.return_value = None
-    mock_response.json.return_value = [
-        {
-            "id": 1,
-            "idUUID": 1,
-            "serviceId": 1,
-            "startDateTimeUtc": "2024-01-01",
-            "endDateTimeUtc": "2024-12-12",
-            "missionTypes": "missionTypes",
-            "controlUnits": "controlUnits",
-            "facade": "facade",
-            "isDeleted": False,
-            "missionSource": "missionSource",
-            "data": {
-                "indicateur1": 1,
-                "indicateur2": 2
+    mock_response.json.return_value = {
+        "results": [
+            {
+                "id": 1,
+                "idUUID": 1,
+                "serviceId": 1,
+                "startDateTimeUtc": "2024-01-01",
+                "endDateTimeUtc": "2024-12-12",
+                "missionTypes": "missionTypes",
+                "controlUnits": "controlUnits",
+                "facade": "facade",
+                "isDeleted": False,
+                "missionSource": "missionSource",
+                "data": {
+                    "indicateur1": 1,
+                    "indicateur2": 2
+                }
             }
-        }
-    ]
+        ]
+    }
     
-    mock_requests_get.return_value = mock_response
+    mock_requests_post.return_value = mock_response
 
     rows = fetch_rapportnav_api.run(
         path="/api/v1/items",
