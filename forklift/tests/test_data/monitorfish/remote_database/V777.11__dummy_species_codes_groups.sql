8a7a8de20536d408082ad997d88165b32dbab6ec
Vincent <vincent.chery@m4x.org>
2026-04-17 11:30:25 +0200

    Add species and gears group tables

---
+++ forklift/tests/test_data/monitorfish/remote_database/V777.11__dummy_species_codes_groups.sql
@@ -0,0 +1,8 @@
+DELETE FROM public.species_codes_groups;
+
+INSERT INTO public.species_codes_groups (
+        species_code, species_group_id) VALUES
+    (          'ANE',               12),
+    (          'HKE',               13),
+    (          'BSS',               13),
+    (          'SOL',               13);
